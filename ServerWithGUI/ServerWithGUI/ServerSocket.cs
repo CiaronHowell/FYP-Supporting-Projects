3258d5b3fcb6f24af05970e0606a4adfeea861e3
Ciaron Howell <ciaron_howell@hotmail.co.uk>
2021-04-22 16:06:44 +0100

    Socket Server with GUI

---
+++ ServerWithGUI/ServerWithGUI/ServerSocket.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerWithGUI
+{
+    public class ServerSocket
+    {
+        private Socket _server;
+
+        public ServerSocket()
+        {
+            _server = new(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+        }
+
+        public void StartServer()
+        {
+            _server.Start
+            _server.Listen()
+        }
+
+        // TODO: Create event so GUI can latch on and display the return value
+
+        // TODO: Call in form
+
+    }
+}
